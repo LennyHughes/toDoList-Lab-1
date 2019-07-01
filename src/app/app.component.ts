cedceccc369ef46a1d5f704e877eaa392f4681bf
Lenny Hughes <lenaudhughes@gmail.com>
2019-07-01 16:45:05 -0400

    todo lab 1 submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+interface Todo {
+  task : string;
+  completed : boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'ToDoList1';
-}
+   
+    taskObject : Todo [] = [
+      {
+        task: "Wash clothes",
+        completed: false
+      },
+      {
+        task: "Cook meals",
+        completed: true
+      },
+      {
+        task: "Pay bills",
+        completed: true
+      }
+    ]
+
+    getListClass(index :number){
+      if(this.taskObject[index].completed){
+        return "completed-li";
+      }
+      else {
+         return "not-completed-li";
+      }  
+    
+    }
+
+   }
+
+   
+
+
